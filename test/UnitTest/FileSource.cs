0270a33048278b84169dddb5d599fc2e1d0a7b6c
Thiago Marcos P. Santos <tmpsantos@gmail.com>
2016-10-15 01:01:19 +0300

    [test] Added unit tests for the Mono FileSource
    Running from the command line is also possible with:
    
    $ nuget restore MapboxSDKUnity.sln
    $ xbuild /p:Configuration=Release MapboxSDKUnity.sln
    $ mono packages/NUnit.ConsoleRunner.3.5.0/tools/nunit3-console.exe bin/Release/Mapbox.UnitTest.dll

---
+++ test/UnitTest/FileSource.cs
@@ -0,0 +1,111 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="FileSource.cs" company="Mapbox">
+//     Copyright (c) 2016 Mapbox. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Mapbox.UnitTest
+{
+    using System;
+    using Mapbox;
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal class FileSource
+    {
+        private const string Uri = "https://api.mapbox.com/geocoding/v5/mapbox.places/helsinki.json";
+        private Mono.FileSource fs;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.fs = new Mono.FileSource();
+        }
+
+        [Test]
+        public void AccessTokenSet()
+        {
+            Assert.IsNotNull(
+                Environment.GetEnvironmentVariable("MAPBOX_ACCESS_TOKEN"),
+                "MAPBOX_ACCESS_TOKEN not set in the environment.");
+        }
+
+        [Test]
+        public void Request()
+        {
+            this.fs.Request(
+                Uri,
+                (Response res) =>
+                {
+                    Assert.IsNotNull(res.Data, "No data received from the servers.");
+                });
+
+            this.fs.WaitForAllRequests();
+        }
+
+        [Test]
+        public void MultipleRequests()
+        {
+            int count = 0;
+
+            this.fs.Request(Uri, (Response res) => ++count);
+            this.fs.Request(Uri, (Response res) => ++count);
+            this.fs.Request(Uri, (Response res) => ++count);
+
+            this.fs.WaitForAllRequests();
+
+            Assert.AreEqual(count, 3, "Should have received 3 replies.");
+        }
+
+        [Test]
+        public void RequestCancel()
+        {
+            var request = this.fs.Request(
+                Uri,
+                (Response res) =>
+                {
+                    Assert.Fail("Should never happen.");
+                });
+
+            request.Cancel();
+
+            this.fs.WaitForAllRequests();
+        }
+
+        [Test]
+        public void RequestDnsError()
+        {
+            this.fs.Request(
+                "https://dnserror.shouldnotwork",
+                (Response res) =>
+                {
+                    Assert.AreEqual(res.Error, "Error: NameResolutionFailure");
+                });
+
+            this.fs.WaitForAllRequests();
+        }
+
+        [Test]
+        public void RequestForbidden()
+        {
+            // Mapbox servers will return a forbidden when attempting
+            // to access a page outside the API space with a token
+            // o the query. Let's hope the behaviour stay like this.
+            this.fs.Request(
+                "https://mapbox.com/forbidden",
+                (Response res) =>
+                {
+                    Assert.AreEqual(res.Error, "Forbidden");
+                });
+
+            this.fs.WaitForAllRequests();
+        }
+
+        [Test]
+        public void WaitWithNoRequests()
+        {
+            // This should simply not block.
+            this.fs.WaitForAllRequests();
+        }
+    }
+}
