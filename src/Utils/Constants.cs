9d5aa4b8ae14438a0d85e5689062a4c2b09412b6
Thiago Marcos P. Santos <thiago@mapbox.com>
2016-10-19 16:48:55 +0300

    [utils] Add the limits of the mercator projection
    Needed when calculation the tiles we need to request based on a bounding
    box.

--- src/Utils/Constants.cs
+++ src/Utils/Constants.cs
@@ -11,5 +11,11 @@ namespace Mapbox
     {
         /// <summary> Base URL for all the Mapbox APIs. </summary>
         public const string BaseAPI = "https://api.mapbox.com/";
+
+        /// <summary> Mercator projection max latitude limit. </summary>
+        public const double LatitudeMax = 85.0511;
+
+        /// <summary> Mercator projection max longitude limit. </summary>
+        public const double LongitudeMax = 180;
     }
 }
