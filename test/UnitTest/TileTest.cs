dffa2600788e92833d1091c2a75e8ab07fcda34e
Thiago Marcos P. Santos <thiago@mapbox.com>
2016-10-26 16:22:49 +0300

    [test] Added unit test for the tile loader API

---
+++ test/UnitTest/TileTest.cs
@@ -0,0 +1,40 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TileTest.cs" company="Mapbox">
+//     Copyright (c) 2016 Mapbox. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+namespace Mapbox.UnitTest
+{
+    using Mapbox.Map;
+    using NUnit.Framework;
+
+    [TestFixture]
+    internal class TileTest
+    {
+        private Mono.FileSource fs;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.fs = new Mono.FileSource();
+        }
+
+        [Test]
+        public void TileLoading()
+        {
+            byte[] data;
+
+            var parameters = new Tile.Parameters();
+            parameters.Fs = this.fs;
+            parameters.Id = new CanonicalTileId(1, 1, 1);
+
+            var tile = new RawPngRasterTile();
+            tile.Initialize(parameters, () => { data = tile.Data; });
+
+            this.fs.WaitForAllRequests();
+
+            Assert.Greater(tile.Data.Length, 1000);
+        }
+    }
+}
