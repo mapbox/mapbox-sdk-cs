e2135b7e308a6317874c6e18f793e12e939aec39
bergwerkgis <wb@bergwerk-gis.at>
2017-03-21 15:08:24 +0100

    start working on multithreaded/multitasked FileSource

--- src/Mono/HTTPRequest.cs
+++ src/Mono/HTTPRequest.cs
@@ -4,65 +4,52 @@
 // </copyright>
 //-----------------------------------------------------------------------
 
-namespace Mapbox.Mono
-{
+namespace Mapbox.Mono {
 	using System;
 	using System.Net.Http;
 	using System.Threading.Tasks;
 
-	internal sealed class HTTPRequest : IAsyncRequest
-	{
+	internal sealed class HTTPRequest : IAsyncRequest {
 		private static readonly HttpClient Client = new HttpClient();
 
-		private Task<Response> task;
-		private Action<Response> callback;
+		private Task<Response> _task;
+		private Action<Response> _callback;
 
-		public HTTPRequest(string url, Action<Response> callback)
-		{
-			this.callback = callback;
-			this.task = this.DoRequestAsync(url);
+		public HTTPRequest(string url, Action<Response> callback) {
+			_callback = callback;
+			_task = DoRequestAsync(url);
 		}
 
-		public void Cancel()
-		{
+		public void Cancel() {
 			// FIXME: CancellationTokenSource not available on Mono?
 			// We should use it when it gets available.
-			this.callback = null;
+			_callback = null;
 		}
 
-		public bool Wait()
-		{
-			if (this.task.IsCompleted)
-			{
-				if (this.callback != null)
-				{
-					this.callback(this.task.Result);
-					this.callback = null;
+		public bool Wait() {
+			if (_task.IsCompleted) {
+				if (_callback != null) {
+					_callback(_task.Result);
+					_callback = null;
 				}
 			}
 
-			return this.callback == null;
+			return _callback == null;
 		}
 
-		private async Task<Response> DoRequestAsync(string url)
-		{
+		private async Task<Response> DoRequestAsync(string url) {
 			var response = new Response();
 
-			try
-			{
+			try {
 				var message = await Client.GetAsync(url);
 
-				if (message.IsSuccessStatusCode)
-				{
+				if (message.IsSuccessStatusCode) {
 					response.Data = await message.Content.ReadAsByteArrayAsync();
-				}
-				else
-				{
+				} else {
 					response.Error = message.StatusCode.ToString();
 				}
 			}
-			catch (Exception exception)
-			{
+			catch (Exception exception) {
 				response.Error = exception.Message;
 			}
 
