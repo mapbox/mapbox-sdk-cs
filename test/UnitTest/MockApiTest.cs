41f1b7b34cf2a321f88e0f9580e2223389aac0cb
bergwerkgis <wb@bergwerk-gis.at>
2017-04-28 16:45:11 +0200

    mock api: hibri/HttpMock seems to work

--- test/UnitTest/MockApiTest.cs
+++ test/UnitTest/MockApiTest.cs
@@ -24,6 +24,11 @@ namespace Mapbox.UnitTest {
 		private FileSource _fs = new FileSource();
 		IHttpServer _mockApi;
 
+		private struct _testUrl {
+			public static string testMockServer1 = "/testmock1";
+			public static string testMockServer2 = "/testmock2";
+			public static string rateLimitHit = "/ratelimithit";
+		}
 
 		[TearDown]
 		public void Finished() {
@@ -36,27 +41,32 @@ namespace Mapbox.UnitTest {
 		[SetUp]
 		public void SetupMockHttp() {
 
-			//_mockApi = new HttpServer(new Uri(_mockBaseUrl));
-
 			var serverFactory = new HttpServerFactory();
 			_mockApi = serverFactory.Get(new Uri(_mockBaseUrl)).WithNewContext();
 
 			_mockApi.Start();
 
-			_mockApi.Stub(r => r.Get("/*"))
-				.Return("{}")
+			_mockApi.Stub(r => r.Get(_testUrl.testMockServer1))
+				.Return(@"{""name"":""first test""}")
 				.AsContentType("application/json")
 				.OK();
+
+			// test status code ('Unavailable For Legal Reasons') not available in .NET HttpStatusCode enum
+			_mockApi.Stub(r => r.Get(_testUrl.testMockServer1)).WithStatus((HttpStatusCode)451);
+
+			_mockApi.Stub(r => r.Get(_testUrl.rateLimitHit))
+				.Return(string.Empty)
+				.WithStatus((HttpStatusCode)429);
 
 		}
 
 
 		[Test]
-		public void First() {
+		public void TestMockServer() {
 
 			Response resp = new Response();
 			_fs.Request(
-				_mockBaseUrl + "/bla/bla"
+				_mockBaseUrl + _testUrl.testMockServer1
 				, (Response r) => {
 					// hm why doesn't nunit work in here? maybe blocking 'WaitForAllRequests'
 					resp = r;
@@ -65,10 +75,45 @@ namespace Mapbox.UnitTest {
 
 			_fs.WaitForAllRequests();
 
-			System.Diagnostics.Debug.WriteLine(resp.StatusCode);
-			Assert.AreEqual(200, resp.StatusCode, "wrong status code");
+			Assert.IsTrue(resp.StatusCode.HasValue, "mock api did not set status code");
+			Assert.AreEqual(200, resp.StatusCode, "mock api returned wrong status code");
+			Assert.AreEqual("application/json", resp.ContentType, "mock api didn't set correct content type");
+			Assert.AreEqual(@"{""name"":""first test""}", System.Text.Encoding.UTF8.GetString(resp.Data), "mock api returned wrong response");
+
+
+			_fs.Request(
+				_mockBaseUrl + _testUrl.testMockServer2
+				, (Response r) => {
+					// hm why doesn't nunit work in here? maybe blocking 'WaitForAllRequests'
+					resp = r;
+				}
+			);
+
+			_fs.WaitForAllRequests();
+
+			Assert.IsTrue(resp.StatusCode.HasValue, "mock api did not set status code");
+			Assert.AreEqual(451, resp.StatusCode, "mock api returned wrong status code");
+
 		}
 
+
+		[Test]
+		public void RateLimitHit() {
+
+			Response resp = new Response();
+			_fs.Request(
+				_mockBaseUrl + _testUrl.rateLimitHit
+				, (Response r) => {
+					// hm why doesn't nunit work in here? maybe blocking 'WaitForAllRequests'
+					resp = r;
+				}
+			);
+
+			_fs.WaitForAllRequests();
+
+			Assert.IsTrue(resp.StatusCode.HasValue, "mock api did not set status code");
+			Assert.AreEqual(429, resp.StatusCode, "rate limit status code not set");
+		}
 
 
 	}
